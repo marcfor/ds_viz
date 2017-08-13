5f6e845278984a7ba878d520c9fd1ec0a37eb078
Marc <fortier.marc@gmail.com>
2017-08-13 08:03:42 -0400

    Got it working with bar charts

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ from flask import render_template
 import json
 import sqlite3
 
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    conn = sqlite3.connect('history.db')
+    c = conn.cursor()
+    c.execute('''select name from collections''')
+    data_sources = c.fetchall()
+    conn.close()
+    return render_template("index.html", data_sources=sorted(data_sources))
 
 
 @app.route("/data")
@@ -24,11 +28,11 @@ def get_data():
     results = []
     for result in c.fetchall():
         results.append({
-            'collection': result[0],
+            'data_source': result[0],
             'timestamp': result[1],
             'count': result[2]
         })
-
+    conn.close()
     return json.dumps(results)
 
 
