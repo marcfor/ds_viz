5f875efd4ed4dbc76a571dd23b41646366c09f1f
Marc <fortier.marc@gmail.com>
2017-08-13 07:58:55 -0400

    Got it working with bar charts

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -2,62 +2,46 @@ queue()
     .defer(d3.json, "/data")
     .await(makeGraphs);
 
+function makeGraph(name, records) {
+	var ndx = crossfilter(records);
+    var dateDim = ndx.dimension(function(d) { return d.timestamp; });
+    var dateGroup = dateDim.group().reduceSum(function(d) {return d.count;});
+
+	var minDate = dateDim.bottom(1)[0]["timestamp"];
+//	minDate.setDate(minDate.getDate() - 1/120);
+	var maxDate = dateDim.top(1)[0]["timestamp"];
+//	maxDate.setDate(maxDate.getDate() + 1/120);
+
+    var dsDim = ndx.dimension(function(d) {return d.data_source});
+    dsDim.filter(name);
+    var chart = dc.barChart('#' + name);
+    chart
+        .width(650)
+        .height(140)
+        .margins({top: 10, right: 50, bottom: 20, left: 50})
+        .dimension(dateDim)
+        .group(dateGroup)
+        .transitionDuration(500)
+        .x(d3.time.scale().domain([minDate, maxDate]))
+        .elasticY(true)
+        .yAxis().ticks(4);
+    chart.render();
+}
+
 function makeGraphs(error, recordsJson) {
-//     alert(recordsJson);
 	//Clean data
 	var records = recordsJson;
 	var dateFormat = d3.time.format("%Y-%m-%d");
-	
+
+	var data_sources = {};
+
 	records.forEach(function(d) {
 		d["timestamp"] = dateFormat.parse(d["timestamp"]);
 		d["timestamp"].setMinutes(0);
 		d["timestamp"].setSeconds(0);
-//		alert(d);
+		data_sources[d.data_source] = true;
 	});
 
-	//Create a Crossfilter instance
-	var ndx = crossfilter(records);
-
-	//Define Dimensions
-	var dateDim = ndx.dimension(function(d) { return d["timestamp"]; });
-	var countDim = ndx.dimension(function(d) { return d["timestamp"]; })
-	var allDim = ndx.dimension(function(d) {return d;});
-
-
-	//Group Data
-	var numRecordsByDate = dateDim.group();
-	var all = ndx.groupAll();
-
-
-	//Define values (to be used in charts)
-	var minDate = dateDim.bottom(1)[0]["timestamp"];
-	var maxDate = dateDim.top(1)[0]["timestamp"];
-
-
     //Charts
-    var numberRecordsND = dc.numberDisplay("#number-records-nd");
-	var timeChart = dc.barChart("#time-chart");
-
-
-
-	numberRecordsND
-		.formatNumber(d3.format("d"))
-		.valueAccessor(function(d){return d; })
-		.group(all);
-
-
-	timeChart
-		.width(650)
-		.height(140)
-		.margins({top: 10, right: 50, bottom: 20, left: 20})
-		.dimension(dateDim)
-		.group(numRecordsByDate)
-		.transitionDuration(500)
-		.x(d3.time.scale().domain([minDate, maxDate]))
-		.elasticY(true)
-		.yAxis().ticks(4);
-
-
-	dc.renderAll();
-
+    Object.keys(data_sources).sort().forEach(function(d) { makeGraph(d, records)});
 };
