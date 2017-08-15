b44a63a3c8dac6c484c356c4c2c8f3ef4d07d826
fortier.marc@gmail.com <N@wkn4wk>
2017-08-15 12:48:02 -0400

    ds viz

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -13,6 +13,13 @@ function graphFilter(group, graph){
     };
 };
 
+// format date as YYYY-mm-dd
+function fmtDate(aDate) {
+    return aDate.getFullYear() + '-'
+            + ('0' + (aDate.getMonth()+1)).slice(-2) + '-'
+            + ('0' + aDate.getDate()).slice(-2);
+};
+
 function makeGraphs(error, recordsJson) {
 	//Clean data
 	var records = recordsJson;
@@ -22,20 +29,19 @@ function makeGraphs(error, recordsJson) {
 
 	records.forEach(function(d) {
 		d["timestamp"] = dateFormat.parse(d["timestamp"]);
-//		d["timestamp"].setMinutes(0);
-//		d["timestamp"].setSeconds(0);
 		graphs[d.graph] = true;
 	});
 
 	var ndx = crossfilter(records);
+	var dtDim = ndx.dimension(function(d) { return d.timestamp; });
+	var minDate = dtDim.bottom(1)[0]["timestamp"];
+//	minDate.setDate(minDate.getDate() - 1/120);
+	var maxDate = dtDim.top(1)[0]["timestamp"];
+//	maxDate.setDate(maxDate.getDate() + 1/120);
+
     var dateDim = ndx.dimension(function(d) { return [d.timestamp, d.data_source, d.graph]; });
     var dateGroup = dateDim.group().reduceSum(function(d) { return d.count; });
 
-	var minDate = dateDim.bottom(1)[0]["timestamp"];
-//	minDate.setDate(minDate.getDate() - 1/120);
-	var maxDate = dateDim.top(1)[0]["timestamp"];
-//	maxDate.setDate(maxDate.getDate() + 1/120);
-    console.log(maxDate);
 
     //Charts
     Object.keys(graphs).sort().forEach(function(aGraph) {
@@ -49,13 +55,14 @@ function makeGraphs(error, recordsJson) {
         var fudge = (maxCount + minCount) / 40;
         var chart = dc.seriesChart("#" + aGraph);
         chart
+            .title(function(d) { return 'x=' + fmtDate(d.key[0]) + ', y=' + d.value; })
+            .brushOn(false) // must be false for the tooltip to work
             .width(500)
             .height(300)
             .margins({top: 20, right: 50, bottom: 50, left: 50})
-            .chart(function(c) { return dc.lineChart(c); })
+            .chart(function(c) { return dc.lineChart(c) .renderDataPoints({radius: 3}); })
             .dimension(dateDim)
             .group(filteredDateGroup)
-    //        .childOptions ({ renderDataPoints: {radius: 20, fillOpacity: 0.8, strokeOpacity: 0.8} })
             .yAxisLabel("Documents")
             .xAxisLabel("Date of reading")
             .xAxisPadding(350)
@@ -66,6 +73,7 @@ function makeGraphs(error, recordsJson) {
             .keyAccessor(function(d) {return +d.key[0];})
             .valueAccessor(function(d) {return +d.value;})
             .legend(dc.legend().x(20).y(0).itemHeight(13).gap(5).itemWidth(250).horizontal(1));
-        chart.render();
     });
+
+    dc.renderAll();
 };
