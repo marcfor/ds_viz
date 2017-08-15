f2dcf62c4c73eed59ea1a7a75b547a0d4ce065c1
Marc <fortier.marc@gmail.com>
2017-08-14 22:08:14 -0400

    tweaking the series graph some more

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -2,22 +2,13 @@ queue()
     .defer(d3.json, "/data")
     .await(makeGraphs);
 
+// return a fake group to filter on graph name
 function graphFilter(group, graph){
     return {
         all:function () {
             return group.all().filter(function(d) {
                 if (d.key[2] == graph) { return d.value; };
             });
-        },
-        maxVal:function () {
-            var max = Number.MIN_SAFE_INTEGER;
-            group.all().forEach(function(d) { if(d.value > max) { max = d.value; } })
-            return max;
-        },
-        minVal:function () {
-            var min = Number.MAX_SAFE_INTEGER;
-            group.all().forEach(function(d) { if(d.value < min) { min = d.value; } })
-            return min;
         }
     };
 };
@@ -31,8 +22,8 @@ function makeGraphs(error, recordsJson) {
 
 	records.forEach(function(d) {
 		d["timestamp"] = dateFormat.parse(d["timestamp"]);
-		d["timestamp"].setMinutes(0);
-		d["timestamp"].setSeconds(0);
+//		d["timestamp"].setMinutes(0);
+//		d["timestamp"].setSeconds(0);
 		graphs[d.graph] = true;
 	});
 
@@ -44,19 +35,24 @@ function makeGraphs(error, recordsJson) {
 //	minDate.setDate(minDate.getDate() - 1/120);
 	var maxDate = dateDim.top(1)[0]["timestamp"];
 //	maxDate.setDate(maxDate.getDate() + 1/120);
+    console.log(maxDate);
 
     //Charts
     Object.keys(graphs).sort().forEach(function(aGraph) {
         var filteredDateGroup = graphFilter(dateGroup, aGraph);
-//        var minCount = filteredDateGroup.bottom(1)[0]["count"];
-//        var maxCount = filteredDateGroup.top(1)[0]["count"];
-        console.log(filteredDateGroup.minVal());
+        var minCount = Number.MAX_SAFE_INTEGER;
+        var maxCount = Number.MIN_SAFE_INTEGER;
+        filteredDateGroup.all().forEach(function(d) {
+            minCount = Math.min(minCount, d.value);
+            maxCount = Math.max(maxCount, d.value);
+        });
+        var fudge = (maxCount + minCount) / 40;
         var chart = dc.seriesChart("#" + aGraph);
         chart
             .width(500)
             .height(300)
             .margins({top: 20, right: 50, bottom: 50, left: 50})
-            .chart(function(c) { return dc.lineChart(c).interpolate('cardinal'); })
+            .chart(function(c) { return dc.lineChart(c); })
             .dimension(dateDim)
             .group(filteredDateGroup)
     //        .childOptions ({ renderDataPoints: {radius: 20, fillOpacity: 0.8, strokeOpacity: 0.8} })
@@ -64,9 +60,8 @@ function makeGraphs(error, recordsJson) {
             .xAxisLabel("Date of reading")
             .xAxisPadding(350)
             .x(d3.time.scale().domain([minDate, maxDate]))
-            .y(d3.scale.linear().domain([filteredDateGroup.minVal(), filteredDateGroup.maxVal()]))
-            .elasticY(true)
-            .yAxis().ticks(4);
+            .y(d3.scale.linear().domain([minCount-fudge, maxCount+fudge]))
+            .yAxis().ticks(5);
         chart.seriesAccessor(function(d) {return d.key[1];})
             .keyAccessor(function(d) {return +d.key[0];})
             .valueAccessor(function(d) {return +d.value;})
